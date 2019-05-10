e6434bac5108a2dd8646efb388d1c6c660da4b0f
Katerina Freeman <katerinafreeman@gmail.com>
2019-05-10 08:57:34 -0700

    added ProductType Model

---
+++ BangazonAPI/Models/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class ProductType
+    {
+        public int Id { get; set;}
+
+        public string Name { get; set; }
+
+
+    }
+}
