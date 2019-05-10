48d2bd33c97f69fc228a06c222a687031a0904cb
Janet Woods <janetnwoods@gmail.com>
2019-05-10 13:28:09 -0500

    paymentTypeModel

---
+++ BangazonAPI/Models/PaymentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class PaymentType
+    {
+        public int id { get; set; }
+
+        public string AcctNumber { get; set; }
+
+        public string Name { get; set; }
+        public int customerId { get; set; }
+
+        public Customer customer { get; set; }
+    }
+}
