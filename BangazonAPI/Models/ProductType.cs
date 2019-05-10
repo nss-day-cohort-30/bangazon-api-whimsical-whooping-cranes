5eee42aac45879e3dc59dcddca4cb178ee4d11e9
Katerina Freeman <katerinafreeman@gmail.com>
2019-05-10 09:00:43 -0700

    name is required in producttype now

--- BangazonAPI/Models/ProductType.cs
+++ BangazonAPI/Models/ProductType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace BangazonAPI.Models
     {
         public int Id { get; set;}
 
+        [Required] 
         public string Name { get; set; }
 
 
