d8999201ffcdab6e69f8b90f0c7f881b58d20678
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-05-10 09:30:38 -0700

    Department Model Completed

--- BangazonAPI/Models/Department.cs
+++ BangazonAPI/Models/Department.cs
@@ -8,5 +8,9 @@ namespace BangazonAPI.Models
     public class Department
     {
         public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Budget { get; set; }
     }
 }
