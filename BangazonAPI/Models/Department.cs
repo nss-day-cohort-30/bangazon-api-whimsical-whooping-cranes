79bd8d5095cc19b44f2c44f467cb0d12df76603c
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-05-10 08:53:26 -0700

    Departmet Model Created

---
+++ BangazonAPI/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string DepartmentName { get; set; }
+        [Required]
+        public int Budget { get; set; }
+    }
+}
