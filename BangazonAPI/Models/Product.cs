b9978d7b68e05e314bd4c776d3adf712681b0e2e
unknown <abbeybrownsemail@gmail.com>
2019-05-10 13:44:41 -0500

    Product Model created

---
+++ BangazonAPI/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public double Price { get; set; } 
+   
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+
+        //This is to hold the actual foreign key integer
+        public int CustomerId { get; set; }
+
+        // This property is for storing the C# object representing the department
+        public Customer Customer { get; set; }
+
+        public int ProductTypeId { get; set; }
+
+        public ProductType ProductType { get; set; }
+
+
+
+    }
+}
